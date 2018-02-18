e9fe0bfc0f213c275596bb74b65f3bf567576135
Ariel Ortega <26ariel.ortega@gmail.com>
2018-02-18 19:26:34 -0300

    Se creó este archivo para poder recuperar la versión del sistema usado y así para poder mostrar en la vista.

---
+++ carritonline/src/java/py/una/pol/par/util/Utils.java
@@ -0,0 +1,13 @@
+package py.una.pol.par.util;
+
+/**
+ * @author Ariel y Fabricio
+ */
+public class Utils {
+    
+    private static final String VERSION = "1.0.0";
+
+    public static String getVERSION() {
+        return VERSION;
+    }
+}
